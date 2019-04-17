a4a845b9c8f8dd6dc9e7a2b51d584d1e0ac4371c
Mauricio Leal <mauriciolealmx@gmail.com>
2019-04-17 17:41:57 -0500

    Created getUserByLocation and editUser queries.

--- src/routes/user/user.queries.js
+++ src/routes/user/user.queries.js
@@ -1,6 +1,19 @@
 const User = require('../../models/user/user');
 
+exports.getUserByLocation = async (lng, lat) =>
+  User.find({
+    geometry: {
+      $near: {
+        $geometry: { type: 'Point', coordinates: [lng, lat] },
+        $minDistance: 0,
+        $maxDistance: 3000
+      }
+    }
+  });
+
 exports.editUser = async (_id, userProps) => {
   await User.findByIdAndUpdate({ _id }, userProps);
   return User.findById(_id);
 };
+
+exports.deleteUser = _id => User.findByIdAndDelete(_id);
