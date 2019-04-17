950c267b93a92f015a17c8d22f8e30ed03e97249
Mauricio Leal <mauriciolealmx@gmail.com>
2019-04-17 17:40:21 -0500

    Added index and deleteUser controllers.

--- src/routes/user/user.controllers.js
+++ src/routes/user/user.controllers.js
@@ -1,8 +1,20 @@
-const { editUser } = require('./user.queries');
+const { getUserByLocation, editUser, deleteUser } = require('./user.queries');
+
+exports.index = async (req, res) => {
+  const { lng, lat } = req.query;
+  const usersByLocation = await getUserByLocation(Number(lng), Number(lat));
+  res.status(200).send(usersByLocation);
+};
 
 exports.edit = async (req, res) => {
   const { id } = req.params;
-
+  
   await editUser(id, req.body);
   res.status(200).json({ success: true });
+};
+
+exports.deleteUser = async (req, res) => {
+  const { id } = req.params;
+  const deletedUser = await deleteUser(id);
+  res.status(200).send(deletedUser);
 };
