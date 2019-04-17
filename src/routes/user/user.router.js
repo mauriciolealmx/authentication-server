24a7739bc279514dbdc04db1a345c628df06db9e
Mauricio Leal <mauriciolealmx@gmail.com>
2019-04-17 17:42:26 -0500

    Adds get and delete user routes.

--- src/routes/user/user.router.js
+++ src/routes/user/user.router.js
@@ -1,5 +1,15 @@
-const { edit } = require('./user.controllers');
+const { index, edit, deleteUser } = require('./user.controllers');
 
 module.exports = app => {
+  /**
+   * POST user does signup. Thus,
+   * that route exists in auth.routes.
+   */
+
+  app.get('/users', index)
+  
   app.put('/users/:id', edit);
+
+  app.delete('/users/:id', deleteUser);
+
 };
