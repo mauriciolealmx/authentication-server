7e24b821f271a9cddbd82f023a64746513d2fe1d
Mauricio Leal <mauriciolealmx@gmail.com>
2019-04-23 15:23:12 -0500

    Added patch to edit meals.

--- src/routes/meal/meal.controllers.js
+++ src/routes/meal/meal.controllers.js
@@ -1,5 +1,4 @@
-const { createMeal, getMeal, getMeals } = require('./meal.queries');
-
+const { createMeal, getMeal, getMeals, editMeal } = require('./meal.queries');
 exports.getMeal = async (req, res) => {
   const { id } = req.params;
 
@@ -31,4 +30,15 @@ exports.createMeal = async (req, res) => {
   if (meal) {
     res.status(200).json(meal);
   }
+};
+
+/**
+ * @method PATCH
+ * @route "/meals/:id"
+ */
+exports.editMeal = async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  await editMeal(id, req.body);
+  res.status(200).json({ success: true });
 };
