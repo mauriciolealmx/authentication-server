84b637b0ef2c4740eba2a98ca8a0c60ab0ec0c5c
Mauricio Leal <mauriciolealmx@gmail.com>
2019-04-29 10:01:49 -0500

    Added comments to auth.routes.

--- src/routes/auth/auth.routes.js
+++ src/routes/auth/auth.routes.js
@@ -2,7 +2,17 @@ const { requireSignin } = require('../../middlewares/passport');
 const { signup, signin } = require('./auth.controllers');
 
 module.exports = function(app) {
+  /**
+   * @route   POST /signin
+   * @desc    Signs in user with email and password
+   * @access  Private
+   */
   app.post('/signin', requireSignin, signin);
 
+  /**
+   * @route   POST /signup
+   * @desc    Creates a user account.
+   * @access  Public
+   */
   app.post('/signup', signup);
 };
