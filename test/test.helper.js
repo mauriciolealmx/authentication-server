daee6bb5e4afcd95ac9bd14cbe92240292cb9d80
Mauricio Leal <mauriciolealmx@gmail.com>
2019-04-30 15:34:52 -0500

    Delete documents in collection instead of dropping the collection.

--- test/test.helper.js
+++ test/test.helper.js
@@ -1,18 +1,33 @@
+/**
+ * Got Random Users from:
+ * fetch('https://randomuser.me/api/?results=20&gender=female&inc=name,email,picture,location').then(res => res.json()).then(console.log)
+ */
+
 const mongoose = require('mongoose');
 const MONGO_URI =
   'mongodb+srv://emdbAdmin:emdbAdmin@email-marketing-kcs7f.mongodb.net/address-book-test?retryWrites=true';
 
 before(async () => {
-  await mongoose.connect(MONGO_URI, { useNewUrlParser: true });
+  await mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true
+  });
+  mongoose.connection.on('open', () => console.log('Connected to mongodb'));
 });
 
 beforeEach(async () => {
-  console.log(`
-    --------------
-    Dropping users
-    --------------
-  `);
+  const { users, meals } = mongoose.connection.collections;
+  await Promise.all([
+    users.deleteMany(), 
+    meals.deleteMany()
+  ]);
 
-  const { users } = mongoose.connection.collections;
-  await users.drop();
+  // Creates indexes if they don't exist yet.
+  users.createIndex({ geometry: '2dsphere' });
+  meals.createIndex({ geometry: '2dsphere' });
+
+  console.log(`
+    ------------------------
+    Deleting Users and Meals
+    ------------------------
+  `);
 });
